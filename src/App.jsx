4a36f462b82b28f1ebc84eda787cfa2b40e8a5ea
Kessy Godoy <kessyon@gmail.com>
2024-02-01 23:22:06 -0300

    Arrumados alguns bugs
    Tinah esquecido de colocar os states

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import SectionForm from './components/SectionForm'
 import Comment from './components/Comment'
 
 function App() {
-  const [comments, setComments] = useState([{date: "01/01/2024",email: "abc@abc.com",comment: "hello!"}])
+  const [comments, setComments] = useState([])
 
-  const addComment = ({email, comment}) => {
-    const newComment = {email, comment}
-    setComments([...comments, newComment])
+  const addComment = (newComment) => {
+    // const newComment = {email, comment}
+    setComments((state) => [newComment, ...state])
   }
 
  
   return (
     <div className="app">
       <SectionForm title={"Seção de Comentários"} addComment={addComment} />
-      {comments.map((comment)=>(
+        {comments.length > 0 ?
+      (comments.map((comment)=>(
         <Comment 
+          key={comment.id}
           email={comment.email}
           comment={comment.comment}  
+          date={comment.date.getDate()}
         />
-      ))}
+      ))):
+        <h4>Nenhum comentário para mostrar...</h4>
+      }
     </div>
   )
 }
--- src/components/SectionForm/index.jsx
+++ src/components/SectionForm/index.jsx
@@ -2,20 +2,31 @@ import { useState } from 'react'
 
 import './styles.css'
 export default function SectionForm({title,addComment}){
-    
+    const [email, setEmail] = useState()
+    const [comment, setComment] = useState()
 
     const submit = (ev)=> {
-        ev.preventDefault();
-        addComment({email, comment})
+        ev.preventDefault()
+
+        const newComment = {
+            key: Math.floor(Math.random() * 1000000),
+            email: email,
+            comment: comment,
+            date: new Date()
+        }
+       
+        console.log(newComment)
+        addComment(newComment)
+        setEmail("")
     }
 
     return(
-        <form action={() => submit}>
+        <form onSubmit={submit}>
             <h3>{title}</h3>
             <label htmlFor="email">Email </label>
-            <input type="email" id="email" />
+            <input type="email" id="email" value={email} onChange={ev => setEmail(ev.target.value) }/>
             <label htmlFor="comment">Comentário</label>
-            <input type="textarea" id="comment"/>
+            <textarea id="comment" value={comment} onChange={ev => setComment(ev.target.value)}></textarea>
             <button>Enviar comentário</button>
             <hr />
         </form>
